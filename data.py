a901cb67df4a8d279c2ba98154628565006a0428
James <4dunsj71@solent.ac.uk>
2021-12-09 17:32:49 +0000

    sp500 pull loop error

--- data.py
+++ data.py
@@ -16,22 +16,42 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, accuracy_score, confusion_matrix, classification_report
 from statsmodels.tsa.stattools import adfuller
 from numpy import log
-tickers=[
-        'aapl',
-        'msft',
-        '1337.hk',
-        'lloy.l',
-        '005930.ks',
-        'jpy=x',
-        'btc-usd',
-        'usdt-btc'
-    ]
+
+columns=['symbol',
+        'security',
+        'filings',
+        'gics',
+        'head',
+        'location',
+        'firstadd',
+        'cik',
+        'founded']
+
+sp500 = read_csv("s&p500.csv",skiprows=[0], names = columns)
+print(sp500.head(5))
+print(sp500['symbol'])
+sp500['symbol'] = sp500['symbol'].str.lower()
+print(sp500.head(5))
+sp500data = pd.DataFrame()
+
+tickers = sp500['symbol']
+print(tickers.head(5))
+ticker = yf.Ticker('mmm')
+print(ticker.history(period='10d', interval='1d'))
+
+for symbol in tickers:
+    ticker = yf.Ticker(symbol)
+    data = ticker.history(period='300d', interval='1d')
+    sp500data[symbol] = data['Close']
+        
+
+
 
 #pulls historical close prices for tickers listed
-for ticker in tickers:
-    ticker_a = yf.Ticker(ticker)
-    data = ticker_a.history(period='300d', interval='1d')
-    data['Close'].to_csv("close{}.csv".format(ticker))
-    print("saving close{}.csv".format(ticker))
+#for ticker in tickers:
+#    ticker_a = yf.Ticker(ticker)
+#    data = ticker_a.history(period='300d', interval='1d')
+#    data['Close'].to_csv("close{}.csv".format(ticker))
+#    print("saving close{}.csv".format(ticker))
 
 
