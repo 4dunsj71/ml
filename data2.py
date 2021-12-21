5f33783bb302d505de8cdcf961d7d878447d6fca
4dunsj71@solent.ac.uk <4dunsj71@solent.ac.uk>
2021-12-21 12:46:41 +0000

    fixing dataframe issues in data2

--- data2.py
+++ data2.py
@@ -6,6 +6,8 @@ import matplotlib
 import csv
 import datetime as dt
 import pmdarima as pm
+import logging
+import sys
 from sklearn import linear_model
 from pmdarima import auto_arima
 from sklearn.linear_model import LinearRegression
@@ -20,55 +22,91 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, no
 from sklearn.cluster import KMeans, SpectralClustering
 from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
+logging.basicConfig(filename='log/log.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
+def my_handler(type, value, tb):
+    logger.exception("{0}".format(str(value)))
+sys.excepthook = my_handler
+sys.stdout=open("log/out","w")
+if __name__ == '__main__':
+    data = read_csv('SnP500Close.csv', header=None, skiprows=1)
 
+    empty = data[data.isna().any(axis=1)]
+    print('empty shape:\n',empty.shape)
+    print('empty length:',len(empty))
+    print('empty:\n',empty)
+    data.fillna(data.mean())
 
-data = read_csv('SnP500Close.csv', header=None, skiprows=1)
+    #data[0] = pd.to_datetime(data[0])
+    #data[0] = data[0].astype('int64').astype(float)
+    #print(data.head(5))
 
-empty = data[data.isna().any(axis=1)]
-print(empty.shape)
-print(len(empty))
-print(empty)
-data.fillna(data.mean(),inplace=True)
+    #transpose to make dates the head for the dataframe, to make kmeans cluster for companies not for dates
+    data = data.T
 
+    #new dataframe created to change date string to datetime, then float for easier calculatioons.
+    dates = data.iloc[0]
+    dates = pd.to_datetime(dates)
+    dates = dates.astype('int64').astype(float)
+    data.iloc[0] = dates
+    data = data.astype(float)
+    #x_data = np.array(data.drop(data[0]).astype(float))
 
-data[0] = pd.to_datetime(data[0])
-data[0] = data[0].astype('int64').astype(float)
-x_data = np.array(data.drop([0],1).astype(float))
-x_data = x_data.round(decimals = 2)
-print(x_data, x_data.shape)
-minmax = MinMaxScaler()
-x_scaled = minmax.fit_transform(x_data)
-print(x_scaled.shape)
+    #np.array(data.drop([0],1).astype(float))
 
-y_data = np.array(data[0])
-print(y_data.shape)
-#----------------------------------------------------------------------------------------------------
-#Elbow Bethod
-#----------------------------------------------------------------------------------------------------
-#clust_list = []
+    #x_data = data
+    #x_data = x_data.drop(data.iloc[0])
+    #print(x_data)
 
-#for cluster in range(1,50):
-#    kmeans = KMeans(n_clusters = cluster, init='k-means++')
-#    kmeans.fit(x_scaled)
-#    clust_list.append(kmeans.inertia_)
+    #np.array(data.drop([0],1).astype(float))
 
-#frame =pd.DataFrame({'Cluster':range(1,50), 'cluster_list':clust_list})
-#plt.plot(frame['Cluster'], frame['cluster_list'], marker = 'o')
-#plt.xlabel('number of clusters')
-#plt.ylabel('Inertia')
-#plt.show()
+    #x_data = x_data.round(decimals = 2)
+    #print('x_data\n: ',x_data, x_data.shape)
+    #minmax = MinMaxScaler()
+    #x_scaled = minmax.fit_transform(x_data)
+    #print('x_scaled shape:\n',x_scaled.shape)
+
+    #y_data = np.array(data[0])
+    #print('y_data:\n', y_data)
+    #print('y_data shape:',  y_data.shape)
+    #----------------------------------------------------------------------------------------------------
+    #Elbow Bethod
+    #----------------------------------------------------------------------------------------------------
+    #clust_list = []
+
+    #for cluster in range(1,50):
+    #    kmeans = KMeans(n_clusters = cluster, init='k-means++')
+    #    kmeans.fit(x_scaled)
+    #    clust_list.append(kmeans.inertia_)
+
+    #frame =pd.DataFrame({'Cluster':range(1,50), 'cluster_list':clust_list})
+    #plt.plot(frame['Cluster'], frame['cluster_list'], marker = 'o')
+    #plt.xlabel('number of clusters')
+    #plt.ylabel('Inertia')
+    #plt.show()
 
 
 #----------------------------------------------------------------------------------------------------
 #Kmeans prediction
 #----------------------------------------------------------------------------------------------------
 
-kmeans = KMeans(n_clusters = 5, init='k-means++')
-kmeans.fit(x_scaled)
-pred = kmeans.predict(x_scaled)
-frame = pd.DataFrame(x_scaled)
-frame['cluster'] = pred
-print(frame['cluster'].value_counts())
+#kmeans = KMeans(n_clusters = 5, init='k-means++')
+#kmeans.fit(x_scaled)
+#pred = kmeans.predict(x_scaled)
+#frame = pd.DataFrame(x_scaled)
+#frame['cluster'] = pred
+#print(frame['cluster'].value_counts())
+    data = data.round(5)
+    kmeans = KMeans(n_clusters = 5, init='k-means++')
+    kmeans.fit(data)
+    pred = kmeans.predict(data)
+    frame = pd.DataFrame(data)
+    frame['cluster'] = pred
+
+    print(frame['cluster'].value_counts())
+stdout.close()
+#
+
 #----------------------------------------------------------------------------------------------------
 #Spectral Clustering
 #----------------------------------------------------------------------------------------------------
